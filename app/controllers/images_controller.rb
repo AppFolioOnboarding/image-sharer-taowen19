a674b523b83e54f85df672f6ae8c6b0ca5d041fa
Taowen Liu <taowen.liu@appfolio.com>
2021-01-13 10:08:49 -0800

    Fixed lint issues.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,11 +2,11 @@ class ImagesController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-      @image = Image.all
+    @image = Image.all
   end
 
   def show
-      @image = Image.find(params[:id])
+    @image = Image.find(params[:id])
   end
 
   def new
@@ -17,11 +17,11 @@ class ImagesController < ActionController::Base
     @image = Image.new(url: params.require(:image)[:url])
 
     if @image.save
-      #redirect_to @image.url
+      # redirect_to @image.url
       render :show
     else
-      puts "save failed"
+      puts 'save failed'
       render :new
     end
   end
-end 
+end
