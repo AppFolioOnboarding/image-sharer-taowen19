74a47df38ce722dc9811b5791a9ff58fc389147a
Taowen Liu <taowen.liu@appfolio.com>
2021-01-15 14:59:56 -0800

    Add 20 default images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Image.create([
+  { url: 'https://i.ytimg.com/vi/0SPwwpruGIA/maxresdefault.jpg' },
+
+  { url: 'https://www.health.harvard.edu/media/content/images/cr/8c38e37d-e8b9-48dd-a9a8-65083a6115e5.jpg' },
+
+  { url: 'https://post.healthline.com/wp-content/uploads/2020/07/pizza-beer-1200x628-facebook-1200x628.jpg' },
+
+  { url: 'https://i.ytimg.com/vi/0SPwwpruGIA/maxresdefault.jpg' },
+
+  { url: 'https://cf.bstatic.com/images/hotel/max1024x768/241/241074099.jpg' },
+
+  { url: 'https://cdn.hswstatic.com/gif/10-breathtaking-views-1-orig.jpg' },
+
+  { url: 'https://www.japan-guide.com/g18/6933_01.jpg' },
+
+  { url: 'https://athletickings.me/wp-content/uploads/2020/10/Tennis-Academy-in-Dubai.jpg' },
+
+  { url: 'https://upload.wikimedia.org/wikipedia/commons/a/a1/Scottsview3.1.jpg' },
+
+  { url: 'https://tennishead.net/wp-content/uploads/2019/11/Novak-Djokovic-Roger-Federer-Wimbledon-2019-watch-ATP-tennis-on-TV-in-2020.jpg' },
+  { url: 'https://media.tacdn.com/media/attractions-splice-spp-674x446/06/73/10/c0.jpg' },
+
+  { url: 'https://dhvu905wjl8ow.cloudfront.net/apm/assets/hero_accounting_reporting_550x357.png' },
+
+  { url: 'https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_d802dea7f1678da63e137f59839ab6b0/appfolio-property-manager.png' },
+
+  { url: 'https://upload.wikimedia.org/wikipedia/commons/a/a1/Scottsview3.1.jpg' },
+
+  { url: 'https://sporttechie-prod.s3.amazonaws.com/2019/09/rafael-nadal-us-open.png' },
+
+  { url: 'https://blog.appfolio.com/wp-content/uploads/2021/01/blog-post-image-1.7.2.jpg' },
+
+  { url: 'https://blog.appfolio.com/wp-content/uploads/2021/01/blog-post-image-12.30_data.jpg' },
+
+  { url: 'https://blog.appfolio.com/wp-content/uploads/2018/08/blog-post-image-12.30.jpg' },
+
+  { url: 'https://blog.appfolio.com/wp-content/uploads/2020/11/blog-post-image-10.20.jpg' },
+
+  { url: 'https://blog.appfolio.com/wp-content/uploads/2020/12/blog-post-image-12.2.jpg' }
+])
