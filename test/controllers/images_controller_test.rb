7d162cf1b6908c98dc7a54fb2737b951b1fd05c5
Taowen Liu <taowen.liu@appfolio.com>
2021-01-20 12:48:48 -0800

    Add tag filter to index page with tests

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -4,6 +4,7 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @valid_image_url = 'https://learn.appfolio.com/apm/www/images/apm-mobile-nav2-logo.png'
     @invalid_image_url = 'https://.com'
+    @default_domain = 'http://www.example.com'
   end
 
   teardown do
@@ -34,6 +35,37 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
           end
         end
       end
+    end
+  end
+
+  def test_index_with_tag_filter__success
+    Image.create!(url: @valid_image_url, id: 1, tag_list: 'tag1')
+    Image.create!(url: @valid_image_url, id: 2, tag_list: 'tag2')
+    assert_equal Image.tagged_with('tag1').count, 1
+
+    get images_url(tag: 'tag2')
+    assert_response :success
+
+    assert_select 'table' do
+      assert_select 'tr:count', 1
+      assert_select 'tr:nth-child(1)' do
+        assert_select 'td' do
+          assert_select 'img' do
+            assert_select '[id=?]', '2'
+            assert_select '[tags=?]', 'tag2'
+          end
+        end
+      end
+    end
+  end
+
+  def test_index_with_invalid_tag__success
+    get images_url(tag: 'tag2')
+    assert_response :success
+
+    assert_select 'p', 'Nonexistent Images'
+    assert_select 'table' do
+      assert_select 'tr:count', 0
     end
   end
 
@@ -72,12 +104,14 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   def test_show__success
-    image = Image.create!(url: @valid_image_url, id: 1, tag_list: 'tag1 tag2')
+    image = Image.create!(url: @valid_image_url, id: 1, tag_list: %w[tag1 tag2])
     get image_url(image)
     assert_response :success
 
-    assert_select 'h1', 'tag1 tag2'
-
+    assert_select 'div' do
+      assert_select 'a[href=?]', @default_domain + '/images?tag=tag1'
+      assert_select 'a[href=?]', @default_domain + '/images?tag=tag2'
+    end
     assert_select 'img' do
       assert_select '[id=?]', '1'
       assert_select '[tags=?]', 'tag1 tag2'
